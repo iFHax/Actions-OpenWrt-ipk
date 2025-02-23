08a9cce5d1f9303e57cb1719e500ad7e04ad4940
YL2209 <3021027861@qq.com>
2025-02-24 06:42:36 +0800

    workflows_all

--- workflows/diy-part.sh
+++ workflows/diy-part.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/YL2209/luci-app-campus-network-login.git package/luci-app-campus-network-login
-./scripts/feeds install luci-app-campus-network-login
+git clone https://github.com/YL2209/luci-theme-argon.git package/luci-theme-argon
+./scripts/feeds install luci-theme-argon
 echo "CONFIG_ALL_NONSHARED=n" > .config
 echo "CONFIG_ALL_KMODS=n" >> .config
 echo "CONFIG_ALL=n" >> .config
 echo "CONFIG_AUTOREMOVE=n" >> .config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-campus-network-login=m" >> .config
+echo "CONFIG_PACKAGE_luci-theme-argon=m" >> .config
