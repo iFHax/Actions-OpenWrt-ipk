9ef6b18ad1fd5cee6c71a104d56d036bd177231b
YL2209 <3021027861@qq.com>
2025-02-24 13:45:54 +0800

    npm publish

--- workflows/diy-part.sh
+++ workflows/diy-part.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/YL2209/luci-theme-argon.git package/luci-theme-argon
-./scripts/feeds install luci-theme-argon
+git clone https://github.com/YL2209/luci-theme-alpha.git package/luci-theme-alpha
+./scripts/feeds install luci-theme-alpha
 echo "CONFIG_ALL_NONSHARED=n" > .config
 echo "CONFIG_ALL_KMODS=n" >> .config
 echo "CONFIG_ALL=n" >> .config
 echo "CONFIG_AUTOREMOVE=n" >> .config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
-echo "CONFIG_PACKAGE_luci-theme-argon=m" >> .config
+echo "CONFIG_PACKAGE_luci-theme-alpha=m" >> .config
