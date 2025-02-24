630b26dc3d60de7464a67abda9cabd6a2dda9041
YL2209 <3021027861@qq.com>
2025-02-24 20:10:55 +0800

    npm publish

--- workflows/diy-part.sh
+++ workflows/diy-part.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/YL2209/luci-theme-alpha.git package/luci-theme-alpha
-./scripts/feeds install luci-theme-alpha
+git clone https://github.com/YL2209/luci-app-campus-network-login.git package/luci-app-campus-network-login
+./scripts/feeds install luci-app-campus-network-login
 echo "CONFIG_ALL_NONSHARED=n" > .config
 echo "CONFIG_ALL_KMODS=n" >> .config
 echo "CONFIG_ALL=n" >> .config
 echo "CONFIG_AUTOREMOVE=n" >> .config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
-echo "CONFIG_PACKAGE_luci-theme-alpha=m" >> .config
+echo "CONFIG_PACKAGE_luci-app-campus-network-login=m" >> .config
