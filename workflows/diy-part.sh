41fd2f63e741e35f2c25b3203970d5529fe0edf5
iFHax <ifhax@proton.me>
2025-04-23 03:02:14 +0800

    Update diy-part.sh

--- workflows/diy-part.sh
+++ workflows/diy-part.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/YL2209/luci-app-campus-network-login.git package/luci-app-campus-network-login
-./scripts/feeds install luci-app-campus-network-login
+git clone https://github.com/iFHaxx/luci-app-ttl.git package/luci-app-ttl
+./scripts/feeds install luci-app-ttl
 echo "CONFIG_ALL_NONSHARED=n" > .config
 echo "CONFIG_ALL_KMODS=n" >> .config
 echo "CONFIG_ALL=n" >> .config
 echo "CONFIG_AUTOREMOVE=n" >> .config
-echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-campus-network-login=m" >> .config
+echo "CONFIG_LUCI_LANG_en=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-ttl=m" >> .config
